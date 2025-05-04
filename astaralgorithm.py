76b2c810f7435326c44aa7309dfd42e67da7beec
SunCh1p <blaha.s.connor@gmail.com>
2025-05-04 18:22:38 -0400

    added comments to astaralgorithm.py

--- astaralgorithm.py
+++ astaralgorithm.py
@@ -4,133 +4,141 @@ import heapq
 
 class Cell:
   def __init__(self):
-    #Parent cell's row index
+    #parent cell's row index
     self.parent_i = 0
-    #Parent cell's column index
+    #parent cell's column index
     self.parent_j = 0
-    #Total cost of the cell (g + h)
+    #total cost of the cell (g + h)
     self.f = float('inf')
-    #Cost from start to this cell
+    #cost from start to this cell
     self.g = float('inf')
-    #Heuristic cost from this cell to destination
+    #heristic cost from current cell to destination
     self.h = 0
 
-# Check if a cell is valid (within the grid)
+#check if a cell is valid
 def is_valid(row, col, rowMax, colMax):
   return (row >= 0) and (row < rowMax) and (col >= 0) and (col < colMax)
 
-# Check if a cell is unblocked
+#check if cell is unblocked
 def is_unblocked(grid, row, col):
   return grid[row][col] == 1
 
-# Check if a cell is the destination
+#check if the cell is the destination
 def is_destination(row, col, dest):
   return row == dest[0] and col == dest[1]
 
-# Calculate the heuristic value of a cell (Euclidean distance to destination)
+#used to calculate heristic for a cell using manhattan distance
 def calculate_h_value(row, col, dest):
   return (abs(row-dest[0])+ abs(col-dest[1]))
 
-# Trace the path from source to destination
+#used to retrieve path from source to destination using the destination and a series of cell parents
 def trace_path(cell_details, dest):
-  print("The Path is ")
   path = []
   row = dest[0]
   col = dest[1]
 
-  # Trace the path from destination to source using parent cells
+  #starting from destination, trace path back to source using parent cells
   while not (cell_details[row][col].parent_i == row and cell_details[row][col].parent_j == col):
+    #add current cell
     path.append((row, col))
+    #change row and col to refelct parent cell
     temp_row = cell_details[row][col].parent_i
     temp_col = cell_details[row][col].parent_j
     row = temp_row
     col = temp_col
 
-  # Add the source cell to the path
+  #source has not yet been added so add source cell
   path.append((row, col))
 
-  # Reverse the path to get the path from source to destination
+  #since current path is from destination to source, we reverse it to get source to destination
   path.reverse()
 
+  #return path so it can be used by the program
   return path
 
 #implementation of A*
 def a_star_search(grid, src, dest, rowMax, colMax):
   res = []
-  # Check if the source and destination are valid
+  #check kf source and destination are valid
   if not is_valid(src[0], src[1], rowMax, colMax) or not is_valid(dest[0], dest[1], rowMax, colMax):
     print("Source or destination is invalid")
     return
 
-  # Check if the source and destination are unblocked
+  #check if they are not blocked
   if not is_unblocked(grid, src[0], src[1]) or not is_unblocked(grid, dest[0], dest[1]):
     print("Source or the destination is blocked")
     return
 
-  # Check if we are already at the destination
+  #check if at destination
   if is_destination(src[0], src[1], dest):
     print("We are already at the destination")
-    return
+    return []
 
-  # Initialize the closed list (visited cells)
-  closed_list = [[False for _ in range(colMax)] for _ in range(rowMax)]
-  # Initialize the details of each cell
+  #initialize the reached list(cells we've already visited)
+  reached = [[False for _ in range(colMax)] for _ in range(rowMax)]
+  #initialize a details grid for all the cells so we can reference it later
   cell_details = [[Cell() for _ in range(colMax)] for _ in range(rowMax)]
 
-  # Initialize the start cell details
+  #intialize the start cell
   i = src[0]
   j = src[1]
+  #total cost is zero
   cell_details[i][j].f = 0
+  #heuristic is initialized to be zero(this will be calculated in a bit)
   cell_details[i][j].g = 0
+  #heuristic is initialized to be zero(this will be calculated in a bit)
   cell_details[i][j].h = 0
   cell_details[i][j].parent_i = i
   cell_details[i][j].parent_j = j
 
-  # Initialize the open list (cells to be visited) with the start cell
-  open_list = []
-  heapq.heappush(open_list, (0.0, i, j))
+  #initialize the frontier(candidate cells to be visited)
+  frontier = []
+  #push the src cell onto the heap while ensuring the frontier follows the format of a heap. It is stored on the heap as a tuple
+  heapq.heappush(frontier, (0.0, i, j))
 
-  # Initialize the flag for whether destination is found
+  #flag for it destination is found or not
   found_dest = False
 
-  # Main loop of A* search algorithm
-  while len(open_list) > 0:
-    # Pop the cell with the smallest f value from the open list
-    p = heapq.heappop(open_list)
+  #loop of search, basically always check if there is stuff on the frontier or not
+  while len(frontier) > 0:
+    #grab cell with smallest f value
+    p = heapq.heappop(frontier)
 
-    # Mark the cell as visited
+    #put current cell in reached list
     i = p[1]
     j = p[2]
-    closed_list[i][j] = True
+    reached[i][j] = True
 
-    # For each direction, check the successors
+    #check each direction the robot can move(up, down, left, right)
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     for dir in directions:
+      #get coordinate of new position
       new_i = i + dir[0]
       new_j = j + dir[1]
 
-      # If the successor is valid, unblocked, and not visited
-      if is_valid(new_i, new_j, rowMax, colMax) and is_unblocked(grid, new_i, new_j) and not closed_list[new_i][new_j]:
-        # If the successor is the destination
+      #checking if we can use the successor 
+      #by checking if it is valid, not a border cell, and hasn't been reached yet
+      if is_valid(new_i, new_j, rowMax, colMax) and is_unblocked(grid, new_i, new_j) and not reached[new_i][new_j]:
+        #if it is destination, retunr the list
         if is_destination(new_i, new_j, dest):
-          # Set the parent of the destination cell
+          #set the parent of the dest
           cell_details[new_i][new_j].parent_i = i
           cell_details[new_i][new_j].parent_j = j
-          # Trace and print the path from source to destination
+          #get the path and return it
           res = trace_path(cell_details, dest)
           found_dest = True
           return res
         else:
-          # Calculate the new f, g, and h values
+          #calculate f, g, and h values for successor
           g_new = cell_details[i][j].g + 1.0
           h_new = calculate_h_value(new_i, new_j, dest)
           f_new = g_new + h_new
 
-          # If the cell is not in the open list or the new f value is smaller
+          #if cell is not on frontier or the f value is smaller than what already exists on the frontier
           if cell_details[new_i][new_j].f == float('inf') or cell_details[new_i][new_j].f > f_new:
-            #Add the cell to the open list
-            heapq.heappush(open_list, (f_new, new_i, new_j))
-            #Update the cell details
+            #add cell to frontier
+            heapq.heappush(frontier, (f_new, new_i, new_j))
+            #update its details
             cell_details[new_i][new_j].f = f_new
             cell_details[new_i][new_j].g = g_new
             cell_details[new_i][new_j].h = h_new
